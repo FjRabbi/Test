be669fdacd8c626eb3853aebac3af74a6d969943
FjRabbi <fajle15-12317@diu.edu.bd>
2021-09-15 16:57:39 +0600

    new class added

--- app/src/main/java/com/example/firstchallnege/AnHistoricalSketch.java
+++ app/src/main/java/com/example/firstchallnege/AnHistoricalSketch.java
@@ -2,13 +2,28 @@ package com.example.firstchallnege;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AnHistoricalSketch extends AppCompatActivity {
+
+    Button backBtn1st;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_an_historical_sketch);
+
+        backBtn1st = findViewById(R.id.backBtn1st);
+        backBtn1st.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(AnHistoricalSketch.this, Book.class);
+                startActivity(intent);
+            }
+        });
     }
 }
